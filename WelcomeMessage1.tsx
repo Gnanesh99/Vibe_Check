4aa484187fd5f8a6c40d3c7c82d51ed9905405de
Gnanesh99 <gorlegnanesh.23.csd@anits.edu.in>
2025-05-29 13:22:33 +0530

    Create WelcomeMessage1.tsx

---
+++ WelcomeMessage1.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+export function WelcomeMessage() {
+  return (
+    <section className="px-4 pb-5 mt-20 w-full text-base leading-6 text-white min-h-[413px]">
+      <p>
+        Your journey to emotional and mental well-being starts here. Let's
+        grow together!
+      </p>
+    </section>
+  );
+}
