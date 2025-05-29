6df2468088126a1ad1110ae411c4f205a9293e3e
Gnanesh99 <gorlegnanesh.23.csd@anits.edu.in>
2025-05-29 13:22:05 +0530

    Create WelcomeHeader1.tsx

---
+++ WelcomeHeader1.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+export function WelcomeHeader() {
+  return (
+    <header className="pt-4 w-full text-3xl font-bold leading-none text-white h-[79px]">
+      <h1 className="px-4 pt-5 pb-96 w-full min-h-[497px]">
+        Welcome to VibeCheck
+      </h1>
+    </header>
+  );
+}
